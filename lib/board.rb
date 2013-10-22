b81c7b99bbea3dc77918ed54d8e89f40f682e371
elliottm <m_elliott@live.com>
2013-10-22 14:06:35 +0100

    Third commit

--- lib/board.rb
+++ lib/board.rb
@@ -36,21 +36,31 @@ class Board
     @battleships << battleships
   end
 
-  def split_coordinates at_coordinates
+  def split_coordinates_to_letter at_coordinates
    element_split =  at_coordinates.split('')
    letter = element_split[0].upcase
   end
 
-  def alphabet_hash letter
+  def split_coordinates_to_number at_coordinates
+   element_split =  at_coordinates.split('')
+   number = element_split[1].to_i
+  end
+
+  def converts_letter_to_index letter
     alpha_hash = {'A' => 0, 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4, 'F' => 5, 'G' => 6, 'H' => 7, 'I' => 8, 'J' => 9, 'K' => 10}
-    key_return = alpha_hash[letter] 
+    column_index = alpha_hash[letter] 
   end
 
-  def alphabet_to_integer letter_string
-    index = letter_string.map {|key| value }
+  def first_board_index at_coordinates
+    column_index = converts_letter_to_index(split_coordinates_to_letter(at_coordinates))
+    col_arr = [column_index]
   end
 
-
+  def second_board_index at_coordinates
+    row_index = split_coordinates_to_number(at_coordinates)    
+    row_arr = [row_index]
+  end
+  
   def opponent_view
   end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,11 +34,40 @@ describe Board do
     board.put_ships_to_board battleships
     expect(board).to have_ships
   end
-
-  it 'has an alphabet hash' do
+  it 'splits user coordinates into a letter' do
     player = double :player
     board = Board.new(player)
-    expect(board.alphabet_hash('B')).to eq 1
+    expect(board.split_coordinates_to_letter('A5')).to eq 'A'
   end
+
+  it 'splits user coordinates into a number' do
+    player = double :player
+    board = Board.new(player)
+    expect(board.split_coordinates_to_number('B5')).to eq 5
+  end
+
+  it 'returns index based on a letter' do
+    player = double :player
+    board = Board.new(player)
+    expect(board.converts_letter_to_index('B')).to eq 1
+  end
+
+  it 'returns first index for empty_board' do
+    player = double :player
+    board = Board.new(player)
+    expect(board.first_board_index('A5')).to eq [0]
+  end
+
+  it 'returns first index for empty_board' do
+    player = double :player
+    board = Board.new(player)
+    expect(board.first_board_index('A5')).to eq [5]
+  end
+
+
+# do i connect alpha_hash method to split_coord as an argument?  
+# or in UI config this?
+
+  
 
 end
